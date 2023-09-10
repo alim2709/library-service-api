c150e81bbc25019570199fda3169cb8f88249f2c
alim2709 <alimselemetow@gmail.com>
2023-09-10 22:48:14 +0300

    Register path for BookViewSet in books/urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from books.views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register("books", BookViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "books"
