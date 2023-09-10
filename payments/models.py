f75e5db7d4a5048297acf97e3abdca7a32f3d56d
alim2709 <alimselemetow@gmail.com>
2023-09-10 18:39:21 +0300

    Add Payment model in payments/models.py

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,23 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+
+    status = models.CharField(choices=StatusChoices.choices, default="PENDING")
+    type = models.CharField(choices=TypeChoices.choices, default="PAYMENT")
+    session_url = models.URLField(max_length=400)
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(
+        max_digits=5, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+
+    def __str__(self) -> str:
+        return f"{self.type}: {self.status} ({self.money_to_pay}USD)"
