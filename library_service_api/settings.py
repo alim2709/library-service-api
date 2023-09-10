0759c55a61cac26b3119e43be61c5bb4fc3a64c1
alim2709 <alimselemetow@gmail.com>
2023-09-10 18:42:58 +0300

    Add borrowings, payments, books, user apps to INSTALLED_APPS in settings.py

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "borrowings",
+    "payments",
+    "books",
+    "user",
 ]
 
 MIDDLEWARE = [
