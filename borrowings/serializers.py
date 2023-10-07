08b35f3fecfd6fabfef35821c71f73a21f1735c6
alim2709 <alimselemetow@gmail.com>
2023-10-07 10:50:33 +0300

    Update BorrowingSerializer & add BorrowingCreateSerializer in borrowings/serializers.py

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,7 +1,10 @@
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from borrowings.models import Borrowing
+from payments.models import Payment
+from payments.stripe_session import create_stripe_session
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -11,18 +14,48 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "id",
             "borrow_date",
             "expected_return_date",
+            "actual_return_date",
             "book",
+            "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "book")
+
+    def validate(self, attrs):
+        data = super(BorrowingCreateSerializer, self).validate(attrs)
+        book = attrs["book"]
+        user = self.context["request"].user
+        pending_payments = Payment.objects.filter(borrowing__user=user).filter(
+            status="Pending"
+        )
+        if pending_payments:
+            raise ValidationError(
+                detail="You have one or more pending payments. You can't make borrowings until you pay for them."
+            )
+        if book.inventory == 0:
+            raise serializers.ValidationError(
+                {"book_inventory": f"{book.title} out of stock at this moment"}
+            )
+        return data
 
     @transaction.atomic()
     def create(self, validated_data):
+        borrowing = Borrowing.objects.create(**validated_data)
         book = validated_data["book"]
         book.inventory -= 1
         book.save()
-        return Borrowing.objects.create(**validated_data)
+
+        create_stripe_session(
+            borrowing, request=self.context["request"], payment_type="Payment"
+        )
+        return borrowing
 
 
-class BorrowingDetailSerializer(BorrowingSerializer):
+class BorrowingDetailSerializer(BorrowingCreateSerializer):
     user = serializers.StringRelatedField(many=False, read_only=True)
     book = serializers.StringRelatedField(many=False, read_only=True)
 
