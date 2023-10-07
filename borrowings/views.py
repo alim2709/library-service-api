ed9735ad16dd1412270fe5f32674e00aaf67f689
alim2709 <alimselemetow@gmail.com>
2023-10-07 10:53:15 +0300

    Update BorrowingViewSet in borrowings/views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -7,7 +7,13 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
+from borrowings.serializers import (
+    BorrowingCreateSerializer,
+    BorrowingDetailSerializer,
+    BorrowingReturnSerializer,
+    BorrowingSerializer,
+)
+from payments.stripe_session import create_stripe_session
 
 
 class BorrowingViewSet(
@@ -20,6 +26,8 @@ class BorrowingViewSet(
     serializer_class = BorrowingSerializer
 
     def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
         if self.action == "retrieve":
             return BorrowingDetailSerializer
         return BorrowingSerializer
@@ -31,7 +39,7 @@ class BorrowingViewSet(
 
     def get_queryset(self):
         queryset = self.queryset
-        if self.action in ("list", "retrieve"):
+        if self.action in ("list", "retrieve") and not self.request.user.is_superuser:
             queryset = queryset.filter(user=self.request.user)
 
         """Filtering by user and is active borrowing"""
@@ -40,8 +48,10 @@ class BorrowingViewSet(
         if user:
             user_id = self._params_to_ints(user)
             queryset = queryset.filter(user_id__in=user_id)
-        if is_active:
+        if is_active and is_active == "True":
             queryset = queryset.filter(actual_return_date__isnull=True)
+        if is_active and is_active == "False":
+            queryset = queryset.filter(actual_return_date__isnull=False)
 
         return queryset
 
@@ -49,12 +59,38 @@ class BorrowingViewSet(
         serializer.save(user=self.request.user)
 
     @transaction.atomic
-    @action(methods=["POST"], detail=True, url_path="borrowing_return")
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="borrowing_return",
+        serializer_class=BorrowingReturnSerializer,
+    )
     def borrowing_return(self, request, pk=None):
         borrowing = self.get_object()
-        user = self.request.user
-        borrowing.actual_return_date = datetime.now()
-        borrowing.book.inventory += 1
-        borrowing.book.save()
-        borrowing.save()
+        book = borrowing.book
+        actual_return_date = datetime.now().date()
+        expected_return_date = borrowing.expected_return_date
+
+        serializer_update = BorrowingReturnSerializer(
+            borrowing,
+            data={"actual_return_date": actual_return_date},
+            partial=True,
+        )
+        serializer_update.is_valid(raise_exception=True)
+        serializer_update.save()
+        book.inventory += 1
+        book.save()
+        if actual_return_date > expected_return_date:
+            overdue_period = (actual_return_date - borrowing.expected_return_date).days
+            session = create_stripe_session(
+                borrowing,
+                self.request,
+                payment_type="Fine",
+                overdue_days=overdue_period,
+            )
+            info = {
+                "message": "Your return is overdue please provide Fine payment",
+                "payment_url": session.url,
+            }
+            return Response(info)
         return Response({"status": "borrowing returned"})
