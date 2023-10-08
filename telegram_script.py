ac0a6cb8657f9964d78a82fbe4fb0a7399295049
alim2709 <alimselemetow@gmail.com>
2023-10-08 23:57:33 +0300

    Implement telegram_script.py file for getting chat_id

---
+++ telegram_script.py
@@ -0,0 +1,23 @@
+import json
+import os
+import django
+import requests
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryAPI.settings")
+django.setup()
+
+
+def get_chat_id(bot_token):
+    url = f"https://api.telegram.org/bot{bot_token}/getUpdates"
+    response = requests.get(url)
+    json_data = response.text
+    dict_data = json.loads(json_data)
+    result = dict_data["result"]
+    last_update = max(result, key=lambda x: x["update_id"])
+    chat = last_update["message"]["chat"]
+    return chat["id"]
+
+
+if __name__ == "__main__":
+    print((get_chat_id(settings.TELEGRAM_BOT_TOKEN)))
